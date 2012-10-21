5ecf5f566004792e3a285e590399950e724ec02b
Per Øyvind <perok@stud.ntnu.no>
2012-10-21 18:32:23 +0200

    Android project setup

---
+++ project/src/com/dyskan/spacegame/Menu.java
@@ -0,0 +1,20 @@
+package com.dyskan.spacegame;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Menu extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_menu, menu);
+        return true;
+    }
+}
